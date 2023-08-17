cd183641419fa2a12921abb41aab9f3d3698938c
Muhammad Daud Raheed <daud.rasheed07@gmail.com>
2023-08-17 11:04:02 +0500

    Add files via upload

---
+++ worldometer.py
@@ -0,0 +1,24 @@
+import scrapy
+
+
+class WorldometerSpider(scrapy.Spider):
+    name = 'worldometer'
+    allowed_domains = ['www.worldometers.info']
+    start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
+
+    def parse(self, response):
+        # Getting all the rows of the table
+        rows = response.xpath('//tr')
+
+        for row in rows:
+            # Extracting title, country names and population
+            # title = response.xpath('//h1/text()').get()
+            countries = row.xpath('./td/a/text()').get()
+            population = row.xpath('./td[3]/text()').get()
+
+            # Return data extracted
+            yield {
+                # 'titles': title,
+                'countries': countries,
+                'population': population,
+            }
